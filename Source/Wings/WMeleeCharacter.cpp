2fecd27efde4809297d0d88acd10515e0f9805c6
keyring <keyrings@163.com>
2017-07-12 23:35:23 +0800

    refactor: add Base Unit class and refactor meleecharacter

--- Source/Wings/WMeleeCharacter.cpp
+++ Source/Wings/WMeleeCharacter.cpp
@@ -2,36 +2,16 @@
 
 #include "Wings.h"
 #include "WMeleeCharacter.h"
-#include "WingsCharacter.h"
+
 
 
 // Sets default values
 AWMeleeCharacter::AWMeleeCharacter()
-    :AttackDamageMin(10)
-    ,AttackDamageMax(15)
-    ,AttackRate(1.0f)
-    ,AttackSightRange(300)
-    ,AttackRange(100)
-    ,AttackBaseSpeed(100)
-    ,AttackSpeed(0)
-    ,BountyExp(20)
-    ,BountyGoldMin(20)
-    ,BountyGoldMax(35)
-    ,MovementSpeed(300.f)
-    ,TimeSinceLastAttack(0.f)
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
-	// Configure character movement
-	GetCharacterMovement()->bOrientRotationToMovement = true; // Face in the direction we are moving..
-	GetCharacterMovement()->RotationRate = FRotator(0.0f, -1.0f, 0.0f); // ...at this rotation rate
-	GetCharacterMovement()->GravityScale = 4.f;
-	GetCharacterMovement()->AirControl = 0.70f;
-	GetCharacterMovement()->JumpZVelocity = 1000.f;
-	GetCharacterMovement()->GroundFriction = 3.f;
-	GetCharacterMovement()->MaxWalkSpeed = MovementSpeed;
-	GetCharacterMovement()->MaxFlySpeed = MovementSpeed;
+
 
 }
 
@@ -47,32 +27,6 @@ void AWMeleeCharacter::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
-    AWingsCharacter *Player = Cast<AWingsCharacter>(UGameplayStatics::GetPlayerPawn(GetWorld(), 0));
-    if (!Player) {
-        return;
-    }
-
-    FVector ToPlayer = Player->GetActorLocation() - GetActorLocation();
-    int32 Distance = ToPlayer.Size();
-
-    if (!IsInSightRange(Distance)) {
-        return;
-    }
-    ToPlayer /= Distance;
-    AddMovementInput(ToPlayer, MovementSpeed*DeltaTime);
-    FRotator ToPlayerRotator = ToPlayer.Rotation();
-    ToPlayerRotator.Pitch = 0;
-    RootComponent->SetWorldRotation(ToPlayerRotator);
-
-    if (IsInAttackRange(Distance)) {
-        if (TimeSinceLastAttack >= (AttackRate*100.f/(AttackBaseSpeed+AttackSpeed))) {
-            Attack();
-            TimeSinceLastAttack = 0.f;
-        }
-        TimeSinceLastAttack += DeltaTime;
-    } else {
-        TimeSinceLastAttack = 0.f;
-    }
 
 
 }
@@ -86,7 +40,6 @@ void AWMeleeCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputCom
 
 void AWMeleeCharacter::Attack()
 {
-    int32 AttackDamage = FMath::RandRange(AttackDamageMin, AttackDamageMax);
-    UE_LOG(WingsAttack, Log, TEXT("send attack damage %d"), AttackDamage);
+    Super::Attack();
 }
 
--- Source/Wings/WMeleeCharacter.h
+++ Source/Wings/WMeleeCharacter.h
@@ -4,51 +4,15 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+#include "WUnitBaseCharacter.h"
 #include "WMeleeCharacter.generated.h"
 
 UCLASS()
-class WINGS_API AWMeleeCharacter : public ACharacter
+class WINGS_API AWMeleeCharacter : public AWUnitBaseCharacter
 {
 	GENERATED_BODY()
 
 public:
-    // Attack
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=npc_unit_attack)
-    int32 AttackDamageMin;
-
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=npc_unit_attack)
-    int32 AttackDamageMax;
-
-    UPROPERTY(EditAnyWhere, BlueprintReadWrite, Category=npc_unit_attack)
-    float AttackRate;
-
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=npc_unit_attack)
-    int32 AttackSightRange;
-
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=npc_unit_attack)
-    int32 AttackRange; 
-
-    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category =npc_unit_attack)
-    int32 AttackBaseSpeed;
-
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=npc_unit_attack)
-    int32 AttackSpeed;
-
-    // Bounty
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=npc_unit_bounty)
-    int32 BountyExp;
-
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=npc_unit_bounty)
-    int32 BountyGoldMin;
-
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=npc_unit_bounty)
-    int32 BountyGoldMax;
-
-    // Movement
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=npc_unit_movement)
-    float MovementSpeed;
-
-    float TimeSinceLastAttack;
 
 
 
@@ -63,12 +27,10 @@ public:
     // Called every frame
 	virtual void Tick(float DeltaTime) override;
 
-    void Attack();
+    void Attack() override;
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
-	FORCEINLINE bool IsInSightRange(int32 Distance){ return Distance < AttackSightRange; }
-    FORCEINLINE bool IsInAttackRange(int32 Distance){ return Distance < AttackRange; }
 	
 };
